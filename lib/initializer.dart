9a9e2a15902baa97e926bb2e4ce5dbda1206f7c7
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-03-18 17:18:11 -0300

    Task #8: Criado o tema light e dark e também adicionado o ScreenUtil para responsividade

--- lib/initializer.dart
+++ lib/initializer.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Initializer{
-  static Future<void> init()async{
+class Initializer {
+  static Future<void> init() async {
     try {
       WidgetsFlutterBinding.ensureInitialized();
-      
+      _setStatusBar();
     } catch (e) {
       print(e);
       rethrow;
     }
+  }
+
+  static void _setStatusBar() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent,
+      ),
+    );
   }
 }
